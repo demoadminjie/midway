a7d1ec4bee05a343a780f8e0be2ea4ada99dc87e
mariocai <mariocai@tencent.com>
2023-04-07 20:52:09 +0800

    init: init the mid-way websocket server.

---
+++ index.js
@@ -0,0 +1,35 @@
+import { WebSocketServer, WebSocket } from 'ws';
+
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const { MID_WAY_PORT, LSTM_SERVER_URL } = process.env;
+
+const wss = new WebSocketServer({ port: MID_WAY_PORT });
+
+wss.on('connection', (ws)=> {
+  const serverSocket = new WebSocket(`ws://${LSTM_SERVER_URL}`);
+
+  serverSocket.addEventListener('open', () => {
+    console.log('Connected to server');
+  });
+
+  serverSocket.addEventListener('message', (event) => {
+    console.log(`Received message from server: ${event.data}`);
+    ws.send(event.data);
+  });
+
+  serverSocket.addEventListener('close', () => {
+    console.log('Disconnected from server');
+  });
+
+  ws.on('message', (data) => {
+    console.log(`Received message from client: ${data}`);
+    serverSocket.send(data);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected');
+    serverSocket.close();
+  });
+});
